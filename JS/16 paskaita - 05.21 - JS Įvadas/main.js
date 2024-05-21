d9e67db19424aa4c274db1195e6e150f1775d5d3
BanRokas <rokas.banaitis@gmail.com>
2024-05-21 11:00:41 +0300

    16 paskaita - Kintamieji

--- JS/16 paskaita - 05.21 - JS Įvadas/main.js
+++ JS/16 paskaita - 05.21 - JS Įvadas/main.js
@@ -40,3 +40,34 @@ console.groupEnd();
 // window.alert('Error or smthin');
 // window.prompt('Koks tavo vardas?');
 
+// Kintamieji
+var sunsVardas = "Reksas";
+let arAlkanas = true;
+const mano_DOB = 1996;
+
+let skaicius = 5;
+let kitasSkaicius = 56.4;
+let skaicius2 = -6;
+let sk0 = 555/5*2+3;
+let sk1 = skaicius2 * -5;
+let randomSkaicius = Math.random(); // grąžina random skaičių nuo 0 iki 1
+console.log(skaicius);
+console.log(sk0);
+console.log(sk1);
+console.log(randomSkaicius);
+
+let zodis0 = 'Labas';
+let zodis1 = "Daugiau nei vienas žodis, viename kintamajame";
+let raide = 'a';
+let backtick = `kairiajame viršutiniame klaviatūros kampe`;
+let neSkaicius = '45';
+let neBoolean = "false";
+// let kabuciuKlaida = "cituoju kažką:"citata""; negalima/klaida
+let kabuciuFix0 = 'cituoju kažką: "citata"';
+let kabuciuFix1 = "cituoju kažką: 'citata'";
+let kabuciuFix2 = `cituoju kažką: 'citata'`;
+console.log(raide);
+console.log(neSkaicius);
+
+let tiesa = true;
+let melas = false;
