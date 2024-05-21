0f1dd600feb49d645f69a007e5beaac21caac903
BanRokas <rokas.banaitis@gmail.com>
2024-05-21 10:03:41 +0300

    16 paskaita - Info išvedimo būdai

--- JS/16 paskaita - 05.21 - JS Įvadas/main.js
+++ JS/16 paskaita - 05.21 - JS Įvadas/main.js
@@ -1,3 +1,42 @@
+console.group("pirma grupė");
 console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.log("Kitas failas");
+console.groupEnd();
 
 document.querySelector("#list").addEventListener("click", () => { console.log("paspaudei"); });
+
+console.groupCollapsed("antra grupė");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.log("Kitas failas2");
+console.groupEnd();
+
+// window.alert('Error or smthin');
+// window.prompt('Koks tavo vardas?');
+
