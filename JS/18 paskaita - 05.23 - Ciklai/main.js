99c430abb5f14baf9a069c4c64f27a04e9283028
BanRokas <rokas.banaitis@gmail.com>
2024-05-23 12:06:51 +0300

    18 paskaita - Ciklai not that basic pvz

--- JS/18 paskaita - 05.23 - Ciklai/main.js
+++ JS/18 paskaita - 05.23 - Ciklai/main.js
@@ -58,3 +58,68 @@ while(random < 8){
   console.log(random);
 }
 console.groupEnd();
+
+//      for praktika
+console.groupCollapsed('for praktika');
+// sukamas ciklas nuo 0 iki 20 kas 3
+for(let i = 0; i < 20; i+=3){
+  console.log(i);
+}
+console.log('----------');
+// sukamas ciklas nuo -10 iki 30 kas 8
+for(let i = -10; i < 30; i+=8){
+  console.log(i);
+}
+console.log('----------');
+// sukamas ciklas nuo 10 iki 0 kas 1
+for(let i = 10; i >= 0; i--){
+  console.log(i);  
+}
+console.log('----------');
+// sukamas ciklas nuo 0 iki 15 kas 1; spausdinti tik lyginius skaičius
+for(let i = 0; i <= 15; i++){
+  if(i !== 0 && i % 2 === 0){
+    console.log(i);
+  }
+}
+console.groupEnd();
+
+console.group('for ciklai su masyvais');
+// console.log(masyvas[0]);
+// console.log(masyvas[1]);
+// console.log(masyvas[2]);
+// console.log(masyvas[3]);
+// console.log(masyvas[4]);
+// console.log(masyvas[5]);
+// console.log(masyvas[6]);
+// console.log(masyvas[7]);
+for(let i = 0; i < masyvas.length; i++){
+  console.log(masyvas[i]);
+}
+console.groupEnd();
+
+let fancyMasyvas = [
+  {
+    vardas: "Rokas",
+    amzius: 27
+  },{
+    vardas: "Rokas1",
+    amzius: 272
+  },{
+    vardas: "Rokas2",
+    amzius: 274
+  },{
+    vardas: "Rokas3",
+    amzius: 257
+  },{
+    vardas: "Rokas4",
+    amzius: 2411
+  },{
+    vardas: "Rokas5",
+    amzius: 327
+  }
+];
+
+for(let i = 0; i <= fancyMasyvas.length - 1; i++){
+  console.log(fancyMasyvas[i].vardas);
+}
