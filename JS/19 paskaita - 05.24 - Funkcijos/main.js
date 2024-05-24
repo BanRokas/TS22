b687ed147b0db432e8b3732f900218f436ee885d
BanRokas <rokas.banaitis@gmail.com>
2024-05-24 11:53:33 +0300

    19 paskaita - Function praktika

--- JS/19 paskaita - 05.24 - Funkcijos/main.js
+++ JS/19 paskaita - 05.24 - Funkcijos/main.js
@@ -48,3 +48,29 @@ function mygtukoNuspaudimas(){
   // labai paprastas ir blogas
   // antraste.parentElement.innerHTML += "<img src='https://www.greenmangaming.com/blog/wp-content/uploads/2022/06/teenage-mutant-ninja-turtles-characters-ranked.jpg' style='height:300px; width:auto'>";
 }
+
+// 1) Padaryk, kad kompiuteris 10 kartų paconsole'intų tavo vardą.
+// 2) Pakoreguok pirmą pratimą, kad tiek vardas, tiek kiek kartų kartos - būtų kintamieji.
+// 3) Prie savo vardo atspaudink ir 'i' raidę, t.y. kelintas ciklas yra (pvz.: "0. Petras", "1. Petras", "2. Petras")...
+
+function vardai(vardas, kartai){
+  for(let i = 0; i < kartai; i++){
+    console.log(i + '. ' + vardas);
+  }
+}
+// vardai('Rokas', 10);
+// vardai('Petras', 30);
+
+function nuo_iki(nuo, iki){
+  if(nuo > iki){
+    for(let i = nuo; i >= iki; i--){
+      console.log(i);
+    }
+  } else if(nuo < iki){
+    for(let i = nuo; i <= iki; i++){
+      console.log(i);
+    }
+  }
+}
+nuo_iki(8,13);
+nuo_iki(15,6);
