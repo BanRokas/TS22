8fabc619ed4c94a69f65d59e1066aefed26aff2d
BanRokas <rokas.banaitis@gmail.com>
2024-05-24 10:02:25 +0300

    19 paskaita - func praktika ir DOM teorija

---
+++ JS/19 paskaita - 05.24 - Funkcijos/main.js
@@ -0,0 +1,24 @@
+// console.log(5+1);
+// console.log(3+8);
+// console.log(9+12);
+
+// function sudetis(sk1, sk2){
+//   console.log(sk1+sk2);
+// }
+// sudetis(5, 1);
+// sudetis(3, 8);
+// sudetis(9, 12);
+
+function sudetis(sk1, sk2){
+  let suma = sk1 + sk2;
+  // console.log(suma);
+  return suma;
+}
+let suma0 = sudetis(5, 1);
+console.log(suma0);
+let suma1 = sudetis(3, 8);
+console.log(suma1);
+let bendraSuma = sudetis(suma0, suma1);
+console.log(bendraSuma);
+
+//  Kreipimasis į DOM
