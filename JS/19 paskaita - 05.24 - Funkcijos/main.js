adeea0fec4cd0a5799c6d0d42d9244bb456c94e3
BanRokas <rokas.banaitis@gmail.com>
2024-05-24 10:54:02 +0300

    19 paskaita - DOM JS pavyzdys

--- JS/19 paskaita - 05.24 - Funkcijos/main.js
+++ JS/19 paskaita - 05.24 - Funkcijos/main.js
@@ -22,3 +22,29 @@ let bendraSuma = sudetis(suma0, suma1);
 console.log(bendraSuma);
 
 //  Kreipimasis į DOM
+let mygtukas = document.querySelector('#test > button');
+// console.log(mygtukas);
+console.dir(mygtukas);
+mygtukas.textContent += ' If You Dare';
+mygtukas.addEventListener('click', mygtukoNuspaudimas);
+function mygtukoNuspaudimas(){
+  console.log('Paspaudei mygtuką');
+  mygtukas.textContent = "I can't believe you've done that...";
+
+  // let antraste = document.querySelector('#test');
+  let antraste = mygtukas.parentElement.children[0];
+  antraste.textContent = 'Keičiame HTML su JS';
+  antraste.style.textAlign = 'center';
+  antraste.style.fontSize = '50px';
+  antraste.style.color = 'red';
+
+  // sudėtinga bet tvarkinga
+  let paveiksliukas = document.createElement('img');
+  paveiksliukas.setAttribute('src', 'https://www.greenmangaming.com/blog/wp-content/uploads/2022/06/teenage-mutant-ninja-turtles-characters-ranked.jpg');
+  paveiksliukas.style.height = '300px';
+  paveiksliukas.style.width = 'auto';
+  antraste.parentElement.appendChild(paveiksliukas);
+
+  // labai paprastas ir blogas
+  // antraste.parentElement.innerHTML += "<img src='https://www.greenmangaming.com/blog/wp-content/uploads/2022/06/teenage-mutant-ninja-turtles-characters-ranked.jpg' style='height:300px; width:auto'>";
+}
