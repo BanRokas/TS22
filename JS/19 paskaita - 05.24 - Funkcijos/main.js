a10997fc62b659a3cc8f31e5e657adf78d4bc302
BanRokas <rokas.banaitis@gmail.com>
2024-05-27 09:08:42 +0300

    20 paskaita - 19psk uzd

--- JS/19 paskaita - 05.24 - Funkcijos/main.js
+++ JS/19 paskaita - 05.24 - Funkcijos/main.js
@@ -1,3 +1,4 @@
+console.groupCollapsed('kartu pamoka');
 // console.log(5+1);
 // console.log(3+8);
 // console.log(9+12);
@@ -74,3 +75,54 @@ function nuo_iki(nuo, iki){
 }
 nuo_iki(8,13);
 nuo_iki(15,6);
+console.groupEnd();
+
+console.group('CAO kartu');
+// CAO
+// 1) Ne visos funkcijos turi return statement (nors dažniausiai taip yra). Sukurkite funkciją, į kurią paduosite vardą ir parodysite panaudodami alert.
+function uzd1(vardas, amzius){
+  // alert(vardas);
+  console.log(vardas);
+}
+uzd1('Rokas', 21);
+uzd1('Petras');
+uzd1('Jonas');
+
+// 2) Taip pat ne visos funkcijos turi parametrus – sukurkite funkciją, kuri sugeneruos ir grąžins random skaičių tarp 1 ir 5.
+function uzd2(){
+  let skaicius = Math.random()*4+1 // 1.000004 -> 4.999996
+  return skaicius;
+}
+console.log(uzd2());
+/*
+  Math.round()
+  0    -> 0.49999 => 0
+  0.5  -> 1.49999 => 1
+  1.5  -> 2.49999 => 2
+  2.5  -> 2.99999 => 3
+*/
+
+// 3) Sukurkite funkciją, kuri paims du parametrus – vardą ir pavardę, tada grąžins sumą šių žodžių ilgių.
+function uzd3(vardas, pavarde){
+  let zodis1Ilgis = vardas.length;
+  let zodis2Ilgis = pavarde.length;
+  let bendrasIlgis = zodis1Ilgis + zodis2Ilgis;
+  return bendrasIlgis;
+  // return vardas.length + pavarde.length;
+}
+let ats3 = uzd3('Rokas','Ban');
+console.log(ats3);
+
+// 4) Sukurkite funkciją, kuri pagal paduotą skaičių grąžins abėcėlės raidę;
+let abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+function uzd4(indeksas){
+  return abc[indeksas-1];
+}
+
+console.log(uzd4(5));
+// console.log(uzd4(0));
+console.log(uzd4(20));
+console.log(uzd4(1));
+
+
+console.groupEnd();
