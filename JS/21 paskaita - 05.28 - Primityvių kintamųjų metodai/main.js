0c0b18aa2ba4e4cbb7ad907183673ce8bb17426a
BanRokas <rokas.banaitis@gmail.com>
2024-05-29 09:02:57 +0300

    21 paskaita - CAO task

--- JS/21 paskaita - 05.28 - Primityvių kintamųjų metodai/main.js
+++ JS/21 paskaita - 05.28 - Primityvių kintamųjų metodai/main.js
@@ -1,7 +1,7 @@
 // document.addEventListener('keydown', (e) => {
 //   console.log(e);
 // });
-
+console.groupCollapsed('string metodai');
 
 let zodis = 'Labas Rytas';
 
@@ -42,6 +42,10 @@ console.log(zodis.split('a')); // ['l', 'b', 's ryt', 's']
 console.log(zodis.toUpperCase()); // LABAS RYTAS
 console.log(zodis.toLowerCase()); // labas rytas
 
+console.groupEnd();
+
+console.groupCollapsed('rimtesnis pvz');
+
 let ilgasString = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, quis quia quidem at esse nisi voluptatibus suscipit sequi, sunt error sint harum, quos voluptates quo eum vitae. Ut in, accusamus dolore, ea magni sit dignissimos quas minus debitis laudantium assumenda! Quis in magni natus. Fugit maiores assumenda impedit accusantium beatae ad minus nisi reprehenderit distinctio, labore sed minima tempora pariatur sit quidem repellat quaerat rerum tenetur veritatis debitis architecto repudiandae obcaecati laudantium deleniti. Officiis illum totam odio quasi minima. Dicta debitis quaerat mollitia sapiente doloremque nisi iste. Labore, laborum odit exercitationem quam excepturi sequi aliquam. Assumenda unde ipsa, alias repudiandae a delectus vero voluptatum corrupti quidem iure veniam illum quam. Ut id in quod? Rem quo, fugit placeat omnis error pariatur quisquam laudantium officiis nam? Illum debitis voluptatibus quos qui ullam. Eligendi, nulla obcaecati quas consectetur rem deserunt maxime a dolor aspernatur laudantium ipsa porro fuga dolore commodi esse laboriosam eos et eaque placeat odit. Dicta, aspernatur recusandae molestias, perspiciatis hic harum consectetur omnis quo nam adipisci et optio at eligendi distinctio facere sequi impedit quaerat quam aliquam velit officia, tempore molestiae? Deleniti maiores consectetur esse quis dignissimos necessitatibus quisquam tempore fugiat excepturi eligendi a, illo sequi aliquam ipsa dolore et saepe veritatis itaque assumenda officiis praesentium! Incidunt deserunt explicabo sequi? Qui, rem! Ad esse earum eos officia quibusdam dolorum quod, nisi voluptate iure numquam soluta, eius quaerat nesciunt veritatis aspernatur sint ullam quae. Vero deserunt sed, quis voluptatem aliquid itaque fugit laborum consequatur totam necessitatibus ipsam asperiores. Officia officiis neque consequatur quae harum nam dignissimos molestiae, libero eum placeat asperiores molestias, repellat vero deleniti optio doloribus praesentium provident vel porro similique aut. Necessitatibus, omnis! Debitis rerum ratione soluta amet numquam possimus harum et nulla totam animi? Corporis aliquid maiores totam. Ipsum beatae labore magni illum. Quas eius quod earum.`;
 
 let ilgasStringMasyve = ilgasString.split('. ');
@@ -54,3 +58,25 @@ console.log(ilgasStringMasyve);
 
 ilgasString = ilgasStringMasyve.join('. ');
 console.log(ilgasString);
+
+console.groupEnd();
+
+// CAO
+
+// Iš Didžiosios Raidės
+// Sukurkite funkciją capitalizeFirstLetter, kuri pavertimas pirmąją kiekvieno žodžio raidę tekste į didžiąją.
+
+let capitalizeFirstLetter = (text) => {
+  // console.log(text);
+  let textArray = text.split(' ');
+  // console.log(textArray);
+  let returnString = '';
+  for(let i = 0; i < textArray.length; i++){
+    // console.log(textArray[i].charAt(0).toUpperCase() + textArray[i].slice(1));
+    returnString+=textArray[i].charAt(0).toUpperCase() + textArray[i].slice(1)+' ';
+  }
+  return returnString.trim();
+  // return text.split(' ').map(zodis => zodis[0].toUpperCase() + zodis.slice(1)).join(' ');
+}
+
+console.log(capitalizeFirstLetter("labas rytas, pasauli!"));
