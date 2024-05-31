2b9725162e2d0fa5043758c00e8c32a5b2930dbf
BanRokas <rokas.banaitis@gmail.com>
2024-05-31 09:58:40 +0300

    24 paskaita - Objektų destr

--- JS/24 paskaita - 05.31 - Destruktūrizavimas ir Metodų Praktika/main.js
+++ JS/24 paskaita - 05.31 - Destruktūrizavimas ir Metodų Praktika/main.js
@@ -1,4 +1,4 @@
-let masyvasDes = ['labas', 24, undefined, false, 5, 'zodis', 5, 6, 8];
+let masyvasDes = ['labas', 24, undefined, [false, 5], 'zodis', 5, 6, 8];
 console.log(masyvasDes);
 // be destr
 let el0 = masyvasDes[0];
@@ -9,5 +9,30 @@ let elRest = masyvasDes.slice(5);
 
 console.log(el0, el1, el2, el3, elRest);
 // su dest
-let [ed0, ed1, ed2 = 'default reikšmė', , ed3, ...edRest] = masyvasDes;
-console.log(ed0, ed1, ed2, ed3, edRest);
+let [ed0, ed1, ed2 = 'default reikšmė', [ed3, ed4], ...edRest] = masyvasDes;
+console.log(ed0, ed1, ed2, ed3, ed4, edRest);
+
+let objektasDes = {
+  vardas: "Kazys",
+  amzius: 55,
+  pavarges: true,
+  batuDydis: 45,
+  vairuotojoTeises: undefined,
+  gyvenamojiVieta: {
+    salis: "Lietuva",
+    miestas: "Kaunas"
+  },
+  pomegiai: ['kartingai', 'krepšinis', 'kalnai', 'žygiai']
+};
+console.log(objektasDes);
+// be destr
+let objVardas = objektasDes.vardas;
+let objMetai = objektasDes.amzius;
+let objTeises = objektasDes.vairuotojoTeises !== undefined ? objektasDes.vairuotojoTeises : 'nežinom';
+let objSalis = objektasDes.gyvenamojiVieta.salis;
+let objPomegis1 = objektasDes.pomegiai[1];
+
+console.log(objVardas, objMetai, objTeises, objSalis, objPomegis1);
+// su destr
+let { vardas, amzius:metai, vairuotojoTeises='nežinom', gyvenamojiVieta:{salis, ...restO}, pomegiai:{1:pomegis, ...restM}, ...restObj } = objektasDes;
+console.log(vardas, metai, vairuotojoTeises, salis, pomegis, restObj, restO, restM);
