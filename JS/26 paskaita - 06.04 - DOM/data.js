ad85bd6c2563239c701186385118b13c3a7ee7ed
BanRokas <rokas.banaitis@gmail.com>
2024-06-04 12:16:06 +0300

    26 paskaita - užduotys

---
+++ JS/26 paskaita - 06.04 - DOM/data.js
@@ -0,0 +1,24 @@
+// data pvz
+const duomenysKartu = [
+  {
+    pavadinimas: 'Mortal Engines',
+    aprasymas: 'In a post-apocalyptic world where cities ride on wheels and consume each other to survive, two people meet in London and try to stop a conspiracy.',
+    paveiksliukoURL: 'https://m.media-amazon.com/images/M/MV5BNzY1MDA2OTQ0N15BMl5BanBnXkFtZTgwMTkzNjU2NTM@._V1_.jpg',
+    paveiksliukoALT: 'mortal engines movie poster'
+  },{
+    pavadinimas: 'Harry Potter and the Half-Blood Prince',
+    aprasymas: `As Harry Potter begins his sixth year at Hogwarts, he discovers an old book marked as "the property of the Half-Blood Prince" and begins to learn more about Lord Voldemort's dark past.`,
+    paveiksliukoURL: 'https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_.jpg',
+    paveiksliukoALT: 'HP and HBP movie poster'
+  },{
+    pavadinimas: 'Transformers: Rise of the Beasts',
+    aprasymas: `During the '90s, a new faction of Transformers - the Maximals - join the Autobots as allies in the battle for Earth.`,
+    paveiksliukoURL: 'https://m.media-amazon.com/images/M/MV5BZTNiNDA4NmMtNTExNi00YmViLWJkMDAtMDAxNmRjY2I2NDVjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg',
+    paveiksliukoALT: 'Transformers RotB movie poster'
+  },{
+    pavadinimas: 'The Matrix',
+    aprasymas: `When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.`,
+    paveiksliukoURL: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg',
+    paveiksliukoALT: 'Matrix movie poster'
+  }
+];
