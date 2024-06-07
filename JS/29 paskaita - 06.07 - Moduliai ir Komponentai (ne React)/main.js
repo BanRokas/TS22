d749c7b17394cee8d67ba7b321e1524a3f9b56e5
BanRokas <rokas.banaitis@gmail.com>
2024-06-07 10:52:04 +0300

    28 paskaita - Moduliai

--- JS/29 paskaita - 06.07 - Moduliai ir Komponentai (ne React)/main.js
+++ JS/29 paskaita - 06.07 - Moduliai ir Komponentai (ne React)/main.js
@@ -1,5 +1,5 @@
-import Paragraph from "./moduliai/Paragraph.js";
-import { randomColor, sudetis } from "./moduliai/helperFunc.js";
+import Par from "./moduliai/Paragraph.js";
+import rnmto, { randomColor as rc, sudetis as plus } from "./moduliai/helperFunc.js";
 
 const divOutput = document.querySelector('#komponentuPvz');
 
@@ -10,13 +10,14 @@ const divOutput = document.querySelector('#komponentuPvz');
 // const par1 = document.createElement('p');
 // par1.textContent = 'Mama, noriu miegot :(';
 
-const par = new Paragraph('Labas rytas');
-const par0 = new Paragraph('Pietų metas kada?');
-const par1 = new Paragraph('Mama, noriu miegot :(');
+const par = new Par('Labas rytas');
+const par0 = new Par('Pietų metas kada?');
+const par1 = new Par('Mama, noriu miegot :(');
 
 divOutput.appendChild(par);
 divOutput.appendChild(par0);
 divOutput.appendChild(par1);
 
-console.log(randomColor());
-console.log(sudetis(5, 4));
+console.log(rc());
+console.log(plus(5, 4));
+console.log(rnmto(999));
--- JS/29 paskaita - 06.07 - Moduliai ir Komponentai (ne React)/moduliai/helperFunc.js
+++ JS/29 paskaita - 06.07 - Moduliai ir Komponentai (ne React)/moduliai/helperFunc.js
@@ -1,9 +1,12 @@
-export function randomNumberTo(to){
-  return Math.floor(Math.random()*to);
+const randomNumberTo = (to) => {
+  return Math.floor(Math.random() * to);
 }
-export function randomColor(){
-  return `rgb(${randomNumberTo(256)},${randomNumberTo(256)},${randomNumberTo(256)})`
+function randomColor() {
+  return `rgb(${randomNumberTo(256)},${randomNumberTo(256)},${randomNumberTo(256)})`;
 }
-export function sudetis(sk1, sk2){
+function sudetis(sk1, sk2) {
   return sk1 + sk2;
 }
+
+export default randomNumberTo;
+export { randomColor, sudetis };
