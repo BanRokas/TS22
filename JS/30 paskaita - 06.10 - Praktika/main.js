cf0423ff9804bd01c37d1080bd781391e965392e
BanRokas <rokas.banaitis@gmail.com>
2024-06-10 12:34:40 +0300

    30 paskaita - Calculator (almost done)

---
+++ JS/30 paskaita - 06.10 - Praktika/main.js
@@ -0,0 +1,14 @@
+import Calculator from "./modules/Calculator.js";
+
+const calcInHTML = document.querySelector('div.calculator');
+
+const calculator = new Calculator(calcInHTML);
+
+console.log(calculator);
+
+
+// calculator.calculator.children[1].querySelectorAll('[data-number]').forEach(el => el.addEventListener('click', () => calculator.addNumber(el.dataset.number)));
+[...calculator.calculator.children[1].children].forEach(el => el.dataset.number && el.addEventListener('click', () => calculator.addNumber(el.dataset.number)));
+
+calculator.calculator.children[1].querySelectorAll('[data-action]').forEach(el => el.addEventListener('click', () => calculator.addAction(el.dataset.action)));
+
--- JS/30 paskaita - 06.10 - Praktika/modules/Calculator.js
+++ JS/30 paskaita - 06.10 - Praktika/modules/Calculator.js
@@ -1,24 +1,44 @@
-class Calculator{
-  constructor(){
+export default class Calculator{
+  constructor(calculator){
+    this.calculator = calculator; // HTML elementas
     this.memory = null; // skaičius saugomas atmintyje
-    this.visual = 0; // skaičius rodomas ekrane
+    this.visual = '0'; // skaičius rodomas ekrane
     this.action = null; // veiksmas, kurį norime atlikti su abiem
   }
 
   renderVisual(){
-
+    this.calculator.children[0].textContent = this.visual;
   }
   addNumber(nr){
-    if(this.visual === 0 && nr === 0){
+    if(this.visual === '0' && nr === '0'){
       return;
-    } else if(this.visual === 0 && nr !== 0 && Number.isInteger(nr)) {
+    } else if(nr === '.' && this.visual.includes('.')){
+      return;
+    } else if(this.visual === '0' && nr !== '0' && nr !== '.') {
       this.visual = nr;
     } else {
       this.visual = (this.visual + nr).toString();
     }
     this.renderVisual();
   }
-  addAction(){
+  addAction(symbol){
+    if(this.action){
+      this.calculate();
+    } else {
+      this.memory = this.visual;
+      this.visual = '0';
+    }
+    this.action = symbol;
+  }
+  calculate(){
+    if(this.action === 'sum'){
+      console.log(this.visual*1, this.memory*1);
+      this.visual = this.visual*1 + this.memory*1;
+    }
+    // like veiksmai
 
+    this.memory = this.visual;
+    this.renderVisual();
+    this.visual = '0';
   }
 }
