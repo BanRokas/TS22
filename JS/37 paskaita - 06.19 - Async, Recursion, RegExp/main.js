dfa66342442684dcdb4f502730b1b20238eff821
BanRokas <rokas.banaitis@gmail.com>
2024-06-19 09:17:51 +0300

    37 paskaita - Async (need html explanation)

---
+++ JS/37 paskaita - 06.19 - Async, Recursion, RegExp/main.js
@@ -0,0 +1,52 @@
+// set timeout
+// setTimeout(
+//   () => {
+//     setTimeout(
+//       () => {
+        
+//       }, 1000
+//     )
+//   }, 1000
+// );
+
+// Promise (resolve)
+// function pazadukas(){
+//   return new Promise((resolve) => {
+//     setTimeout( () => {
+//       resolve('Grąžinau kažkokią info');
+//     }, 1000); // valanda būtų 3_600_000
+//   });
+// }
+// console.log(pazadukas());
+// pazadukas().then(response => console.log(response));
+// console.log('labas rytas');
+
+// Promise (resolve, reject)
+const fancyPazadas = (time, data) => {
+  return new Promise((resolve, reject)=>{
+    if(typeof(time) !== 'number'){
+      reject(`Error: ${time} must be a number.`);
+    } else if(time < 0 || time > 10000){
+      reject(`Error: ${time} must be positive number and lower than 10000.`);
+    } else {
+      setTimeout(() => {
+        resolve(`${data} with some changes.`);
+      }, time);
+    }
+  });
+}
+// fancyPazadas(5000, 'duomenys')
+//   .then(response => console.log('all good', response))
+//   .catch(error => console.log('beda', error))
+//   .finally(() => console.log('finally vykdosi visais atvejais.'));
+
+(async () =>{
+  try {
+    const atsakymas = await fancyPazadas(5000, 'duomenys');
+    console.log(atsakymas);
+  } catch(error) {
+    console.log('beda', error);
+  } finally {
+    console.log('finally vykdosi visais atvejais.');
+  }
+})();
