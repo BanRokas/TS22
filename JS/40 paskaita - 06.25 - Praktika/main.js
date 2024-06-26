7d1eb5b9e7cf59e10e984b5430d509a5837cada8
BanRokas <rokas.banaitis@gmail.com>
2024-06-26 10:37:30 +0300

    41 paskaita - add new movie form (with functionality)

--- JS/40 paskaita - 06.25 - Praktika/main.js
+++ JS/40 paskaita - 06.25 - Praktika/main.js
@@ -24,3 +24,52 @@ document
       .querySelector('#newMovieModal')
       .showModal();
   });
+
+document
+  .querySelector('#newMovieModal > .upTopAbs')
+  .addEventListener('click', e => {
+    console.dir(e.target);
+    e.target.previousElementSibling.reset();
+    e.target.parentElement.close();
+  });
+
+document
+  .querySelector('#newMovieForm')
+  .addEventListener('submit', e => {
+    e.preventDefault();
+    const ete = e.target.elements;
+    // console.log(ete);
+    const formInputs = {
+      id: Date.now().toString(),
+      title: ete.title.value,
+      releaseYear: ete.releaseYear.valueAsDate.getFullYear(),
+      eirinCategory: ete.eirinCategory.value,
+      length: ete.duration.valueAsNumber,
+      IMDB:{
+        totalScore: ete.imdbScore.valueAsNumber
+      },
+      photos:{
+        poster: [ete.thumbnail.value]
+      },
+      videos:{
+        trailers: [ete.trailer.value]
+      },
+      description: ete.description.value,
+      genres: [...ete.genres.children].filter(el => el.selected).map(el => el.value)
+    }
+    // console.log(formInputs);
+
+    const cardDiv = new MovieCard(formInputs);
+    document.querySelector('#moviesTP').appendChild(cardDiv);
+
+    fetch(`http://localhost:3000/movies`,{
+      method: "POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(formInputs)
+    });
+
+    e.target.reset();
+    e.target.parentElement.close();
+  });
