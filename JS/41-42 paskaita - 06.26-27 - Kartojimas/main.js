89981a54f71604c0f995fc4f5fc2f8e07a09c80a
BanRokas <rokas.banaitis@gmail.com>
2024-06-26 12:07:39 +0300

    41 paskaita - Kartojimas

---
+++ JS/41-42 paskaita - 06.26-27 - Kartojimas/main.js
@@ -0,0 +1,69 @@
+// Kintamieji
+let zodis = 'zodis kažkoks';
+const raideA = 'A';
+const raideB = "B";
+const raideC = `B`;
+
+let skaicius = 654;
+const suma = 654+666;
+const skirtumas = suma-skaicius;
+
+let tiesa = true;
+const melas = false;
+
+// Duomenų struktūros
+const asmuo = {
+  vardas: "Rokas",
+  pavarde: "Banaitis",
+  amzius: 27,
+  alkanas: true,
+  hobiai: ['dviraičiai', 'žaidimai'],
+  hobiaiIssamiai: [
+    {
+      pavadinimas: 'Dviračiai',
+      paaiskinimas: 'Važinėti dviračiais',
+      kiekLaikoUzsiema: '5 metai'
+    }
+  ],
+  darboviete:{
+    imone:'',
+    adresas:'',
+    uzdarbis: 654,
+  }
+};
+const asmenys = [
+  {
+    vardas: "Rokas",
+    pavarde: "Banaitis",
+    amzius: 27,
+    alkanas: true
+  },{
+    vardas: "Jonas",
+    pavarde: "Jonaitis",
+    amzius: 10,
+    alkanas: false
+  },{
+    vardas: "Petras",
+    pavarde: "Petraitis",
+    amzius: 60,
+    alkanas: true
+  }
+];
+
+// Operatoriai
+5 == '5' // true
+5 === '5' // false
+skaicius = skaicius + 10; // long
+skaicius += 10; // short
+
+// Sąlygos
+if(true){
+  // vyksta
+} else {
+  // nevyksta
+}
+
+// Ciklai
+
+
+// Funkcijos 
