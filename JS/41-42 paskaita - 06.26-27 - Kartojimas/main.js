abaa4fe550690eb432d14db6942b0ee203a2de56
BanRokas <rokas.banaitis@gmail.com>
2024-06-27 09:12:29 +0300

    42 paskaita - Kartojimas

--- JS/41-42 paskaita - 06.26-27 - Kartojimas/main.js
+++ JS/41-42 paskaita - 06.26-27 - Kartojimas/main.js
@@ -66,4 +66,64 @@ if(true){
 // Ciklai
 
 
-// Funkcijos 
+// Funkcijos
+function funcVardas(index){
+  const abc = ['a','b','c'];
+  console.log(abc[index]);
+}
+funcVardas(1);
+const funcVardas0 = function(){
+  console.log('bevarde simple', abc);
+}
+const funcVardas1 = () => {
+  console.log('bevarde arrow');
+}
+const funcVardas2 = (par1, par2) => {
+  return par1 + par2;
+}
+const funcAts = funcVardas2(5, 6);
+console.log(funcAts);
+console.log(funcVardas2(2, 1));
+
+// Metodai
+Math.random(); // duomens tipo metodas (statinis)
+'tekstas'.slice(); // duomens metodas (prototipinis)
+
+// Forma
+// document
+//   .querySelector('formSelector')
+//   .addEventListener('submit', e => {
+//     // neleidžiame numatytojo
+//     e.preventDefault();
+//     // susirenkame reikšmes
+//     const formosReiksmes = {
+//       pirmaReiksme: document.querySelector('formisInputoSelector').value,
+//       antraReiksme: e.target.elements.inputNameOrId.value
+//     }
+//     // arba
+//     const formosReiksmesWillNeedUpdate = new FormData(e.target);
+//     formosReiksmesWillNeedUpdate = formosReiksmesWillNeedUpdate.map((value, key) => { 
+//       return { [key]: value}
+//     });
+//   });
+
+// DOM
+
+// Klasės
+class KlasesVardas{
+  constructor(pilnasVardas){
+    this.vardas = pilnasVardas.slice(0, pilnasVardas.indexOf(' '));
+    this.pavarde = pilnasVardas.slice(pilnasVardas.indexOf(' ')+1);
+  }
+  sveikinasiSu(vardas){
+    return this.vardas + ' sako labas ' + vardas;
+  }
+  static statinisMetodas(sk){
+    return 'kazkas nesusije su konstruktoriumi'.repeat(sk);
+  }
+}
+const klasesTest = new KlasesVardas('Petras Petrauskas');
+const klasesTest0 = new KlasesVardas('As Kas');
+console.log(klasesTest, klasesTest0);
+console.log(klasesTest.sveikinasiSu('Rokas'));
+console.log(KlasesVardas.statinisMetodas(5));
