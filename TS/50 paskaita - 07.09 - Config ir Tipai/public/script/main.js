52c9073e8281ed91f07542a7ab61a853f1b8ea54
BanRokas <rokas.banaitis@gmail.com>
2024-07-09 11:23:04 +0300

    50 paskaita - TypeScript Array ir Tuples

--- TS/50 paskaita - 07.09 - Config ir Tipai/public/script/main.js
+++ TS/50 paskaita - 07.09 - Config ir Tipai/public/script/main.js
@@ -1,2 +1,19 @@
 "use strict";
 console.log('hello world');
+let vardas = 'Rokas';
+let vardas0 = 'Rokas';
+let asmuo = ['vardas', 'pavarde'];
+let asmuo0 = ['vardas', 'pavarde'];
+let asmuo1;
+asmuo1 = ['vardas', 'pavarde'];
+let trysPomegiai = ['1', '2', '3'];
+let asmuo2 = ['Rokas', 'Banaitis', 27, false];
+let masina0 = ['Fiat', 'Punto', 'mėlyna', 'benzinas'];
+let masina00 = ['Fiat', 'Punto', 'mėlyna', 'benzinas'];
+let masina1 = ['Fiat', 'Punto', 'mėlyna', 'benzinas'];
+let masina2 = ['Fiat', 'Punto', 'mėlyna', 'benzinas', 2002];
+let matrica = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
--- TS/50 paskaita - 07.09 - Config ir Tipai/src/main.ts
+++ TS/50 paskaita - 07.09 - Config ir Tipai/src/main.ts
@@ -1,1 +1,31 @@
 console.log('hello world');
+// overdone assignment
+let vardas:string = 'Rokas';
+// good assignment
+let vardas0 = 'Rokas';
+
+// not Tuple, but string array
+let asmuo = ['vardas', 'pavarde'];
+// Tuples
+let asmuo0: [string, string] = ['vardas', 'pavarde'];
+let asmuo1: [string, string];
+asmuo1 = ['vardas', 'pavarde'];
+let trysPomegiai: [string, string, string] = ['1','2','3'];
+// asmuo2 susideda iš: vardo, pavardės, amžiaus ir arAlkanas
+let asmuo2: [string, string, number, boolean] = ['Rokas', 'Banaitis', 27, false];
+
+// Array
+// string'ų masyvas
+let masina0: string[] = ['Fiat', 'Punto', 'mėlyna', 'benzinas'];
+// string'ų masyvas
+let masina00: Array<string> = ['Fiat', 'Punto', 'mėlyna', 'benzinas'];
+// number masyvas arba string masyvas
+let masina1: string[] | number[] = ['Fiat', 'Punto', 'mėlyna', 'benzinas'];
+// masyvas susidedantis iš string ir/arba number
+let masina2: (string | number)[] = ['Fiat', 'Punto', 'mėlyna', 'benzinas', 2002];
+// tikrai nereikia atsiminti
+let matrica: number[][] = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+];
