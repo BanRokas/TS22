99dca499d67e1924d09300d56bc2ea9346b658e0
BanRokas <rokas.banaitis@gmail.com>
2024-07-15 12:23:08 +0300

    54 paskaita - card editing

--- TS/54 paskaita - 07.15 - Praktika/public/script/FootballMatches.js
+++ TS/54 paskaita - 07.15 - Praktika/public/script/FootballMatches.js
@@ -14,14 +14,19 @@ export default class FootballMatches {
         cardDiv.classList.add('footballCard');
         const team1Name = document.createElement('h3');
         team1Name.textContent = this.team1;
+        team1Name.addEventListener("click", () => this.changeStuff(team1Name, "text", "team1"));
         const team1Score = document.createElement('span');
         team1Score.textContent = ((_a = this.team1goals) === null || _a === void 0 ? void 0 : _a.toString()) || 'yet to play';
+        team1Score.addEventListener("click", () => this.changeStuff(team1Score, "number", "team1goals"));
         const team2Name = document.createElement('h3');
         team2Name.textContent = this.team2;
+        team2Name.addEventListener("click", () => this.changeStuff(team2Name, "text", "team2"));
         const team2Score = document.createElement('span');
         team2Score.textContent = ((_b = this.team2goals) === null || _b === void 0 ? void 0 : _b.toString()) || 'yet to play';
+        team2Score.addEventListener("click", () => this.changeStuff(team2Score, "number", "team2goals"));
         const matchDate = document.createElement('p');
         matchDate.textContent = this.dateOfMatch;
+        matchDate.addEventListener("click", () => this.changeStuff(matchDate, "date", "dateOfMatch"));
         const editButton = document.createElement('button');
         editButton.textContent = 'click to edit card';
         editButton.addEventListener('click', () => {
@@ -33,9 +38,32 @@ export default class FootballMatches {
                 this.isBeingEdited = false;
                 editButton.textContent = 'click to edit card';
             }
-            console.log('ar galiu redaguot:', this.isBeingEdited);
         });
         cardDiv.append(team1Name, team1Score, matchDate, team2Name, team2Score, editButton);
         return cardDiv;
+    }
+    changeStuff(element, type, keyName) {
+        var _a;
+        if (this.isBeingEdited) {
+            const elementAsInput = document.createElement('input');
+            elementAsInput.type = type;
+            elementAsInput.value = element.textContent || '';
+            (_a = element.parentElement) === null || _a === void 0 ? void 0 : _a.replaceChild(elementAsInput, element);
+            elementAsInput.focus();
+            elementAsInput.addEventListener('blur', () => {
+                var _a;
+                element.textContent = elementAsInput.value;
+                (_a = elementAsInput.parentElement) === null || _a === void 0 ? void 0 : _a.replaceChild(element, elementAsInput);
+                fetch(`http://localhost:3000/europosFutbolo%C4%8CempionatoRungtyn%C4%97s/${this.id}`, {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        [keyName]: type === 'number' ? elementAsInput.valueAsNumber : elementAsInput.value
+                    })
+                });
+            });
+        }
     }
 }
--- TS/54 paskaita - 07.15 - Praktika/src/FootballMatches.ts
+++ TS/54 paskaita - 07.15 - Praktika/src/FootballMatches.ts
@@ -31,16 +31,23 @@ export default class FootballMatches{
 
     const team1Name = document.createElement('h3');
     team1Name.textContent = this.team1;
+    team1Name.addEventListener("click", () => this.changeStuff(team1Name, "text", "team1"));
+
     const team1Score = document.createElement('span');
     team1Score.textContent = this.team1goals?.toString() || 'yet to play';
+    team1Score.addEventListener("click", () => this.changeStuff(team1Score, "number", "team1goals"));
 
     const team2Name = document.createElement('h3');
     team2Name.textContent = this.team2;
+    team2Name.addEventListener("click", () => this.changeStuff(team2Name, "text", "team2"));
+
     const team2Score = document.createElement('span');
     team2Score.textContent = this.team2goals?.toString() || 'yet to play';
+    team2Score.addEventListener("click", () => this.changeStuff(team2Score, "number", "team2goals"));
 
     const matchDate = document.createElement('p');
     matchDate.textContent = this.dateOfMatch;
+    matchDate.addEventListener("click", () => this.changeStuff(matchDate, "date", "dateOfMatch"));
 
     const editButton = document.createElement('button');
     editButton.textContent = 'click to edit card';
@@ -52,11 +59,33 @@ export default class FootballMatches{
         this.isBeingEdited = false;
         editButton.textContent = 'click to edit card';
       }
-      console.log('ar galiu redaguot:', this.isBeingEdited);
+      // console.log('ar galiu redaguot:', this.isBeingEdited);
     });
-
 
     cardDiv.append(team1Name, team1Score, matchDate, team2Name, team2Score, editButton);
     return cardDiv;
+  }
+  changeStuff(element:Element, type: 'number' | 'text' | 'date', keyName: string){
+    if(this.isBeingEdited){
+      const elementAsInput = document.createElement('input');
+      elementAsInput.type = type;
+      elementAsInput.value = element.textContent || '';
+      element.parentElement?.replaceChild(elementAsInput, element);
+      elementAsInput.focus();
+
+      elementAsInput.addEventListener('blur', () => {
+        element.textContent = elementAsInput.value;
+        elementAsInput.parentElement?.replaceChild(element, elementAsInput);
+        fetch(`http://localhost:3000/europosFutbolo%C4%8CempionatoRungtyn%C4%97s/${this.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type":"application/json"
+          },
+          body: JSON.stringify({
+            [keyName]: type === 'number' ? elementAsInput.valueAsNumber : elementAsInput.value
+          })
+        })
+      });
+    }
   }
 }
