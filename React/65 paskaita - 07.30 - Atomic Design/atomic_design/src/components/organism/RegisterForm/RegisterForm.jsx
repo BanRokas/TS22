cd02adb44063eebeccdb0cf8775e138933e0fa85
BanRokas <rokas.banaitis@gmail.com>
2024-07-30 12:13:14 +0300

    65 paskaita - Register

--- React/65 paskaita - 07.30 - Atomic Design/atomic_design/src/components/organism/RegisterForm/RegisterForm.jsx
+++ React/65 paskaita - 07.30 - Atomic Design/atomic_design/src/components/organism/RegisterForm/RegisterForm.jsx
@@ -6,54 +6,107 @@ import Input from "../../atoms/Input/Input";
 
 const RegisterForm = () => {
 
+  const [formError, setFormError] = useState('');
   const [formInputs, setFormInputs] = useState({
     username: '',
     email: '',
-    gender: ''
+    gender: '',
+    password: '',
+    passwordRepeat: ''
   });
-  const onChangeF = () => {
+  const onChangeF = (e) => {
+    setFormInputs({
+      ...formInputs,
+      [e.target.name]: e.target.type === 'number' ? e.target.valueAsNumber : e.target.value
+    });
+  }
+  const formSubmit = (e) => {
+    e.preventDefault();
+    if(Object.keys(formInputs).some(el => formInputs[el] === '')){ // ar bent viena šio objekto reikšmė yra tuščia
+      setFormError('All fields must be filled');
+    } else if(formInputs.password !== formInputs.passwordRepeat){ // ar vienodi password ir passwordRepeat
+      setFormError('Password and Password Repeat must match');
+    } else {
+      console.log('yay');
 
+      fetch(`https://localhost:8080/users`, {
+        method: "POST",
+        headers:{
+          "Content-Type":"appliccation/json"
+        },
+        body: JSON.stringify({
+          username: formInputs.username,
+          email: formInputs.email,
+          gender: formInputs.gender,
+          password: formInputs.password
+        })
+      })
+    }
   }
 
   return (
-    <form>
-      <InputField
-        name="username" id="username"
-        type="text"
-        text="User name:"
-        placeholderText="Mr. Poopy Pants"
-        value={formInputs.username}
-        onChangeF={onChangeF}
-      />
-      <InputField 
-        name="email" id="email"
-        type="email"
-        text="Email:"
-        placeholderText="something@gmail.com"
-        value={formInputs.email}
-        onChangeF={onChangeF}
-      />
-      <div>
-        <Label
-          text="Choose Gender"
-        />
+    <>
+      <form onSubmit={formSubmit}>
         <InputField
-          name="gender" id="male"
-          type="radio"
-          text="Male"
-          value="male"
+          name="username" id="username"
+          type="text"
+          text="User name:"
+          placeholderText="Mr. Poopy Pants"
+          value={formInputs.username}
+          onChangeF={onChangeF}
+        />
+        <InputField 
+          name="email" id="email"
+          type="email"
+          text="Email:"
+          placeholderText="something@gmail.com"
+          value={formInputs.email}
+          onChangeF={onChangeF}
+        />
+        <div>
+          <Label
+            text="Choose Gender"
+          />
+          <InputField
+            name="gender" id="male"
+            type="radio"
+            text="Male"
+            value="male"
+            onChangeF={onChangeF}
+          />
+          <InputField
+            name="gender" id="female"
+            type="radio"
+            text="Female"
+            value="female"
+            onChangeF={onChangeF}
+          />
+        </div>
+        <InputField
+          name="password" id="password"
+          type="password"
+          text="Password:"
+          placeholderText="Create the password..."
+          value={formInputs.password}
           onChangeF={onChangeF}
         />
         <InputField
-          name="gender" id="female"
-          type="radio"
-          text="Female"
-          value="female"
+          name="passwordRepeat" id="passwordRepeat"
+          type="password"
+          text="Repeat password:"
+          placeholderText="Repeat the password..."
+          value={formInputs.passwordRepeat}
           onChangeF={onChangeF}
         />
-      </div>
-
-    </form>
+        <Input
+          type="submit"
+          value="Register"
+        />
+      </form>
+      {
+        formError && <p>{formError}</p>
+      }
+    </>
   );
 }
  
