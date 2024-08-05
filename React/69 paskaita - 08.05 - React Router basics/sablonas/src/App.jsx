a9e6e5a8fc116f167a74fe3fb3ea8c409ca402f9
BanRokas <rokas.banaitis@gmail.com>
2024-08-05 09:49:35 +0300

    69 paskaita - Router basics (bad file structure)

--- React/69 paskaita - 08.05 - React Router basics/sablonas/src/App.jsx
+++ React/69 paskaita - 08.05 - React Router basics/sablonas/src/App.jsx
@@ -1,6 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+
 const App = () => {
   return (
-    <> </>
+    <>
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/shop">Shop</Link>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/shop" element={<h1>Our Products</h1>} />
+          <Route path="*" element={<h1>Error 404</h1>} />
+        </Routes>
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
