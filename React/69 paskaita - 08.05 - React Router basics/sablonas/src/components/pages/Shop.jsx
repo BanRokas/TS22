7cfda29b15220e4de8e55495b0054ffcf0579584
BanRokas <rokas.banaitis@gmail.com>
2024-08-05 11:11:08 +0300

    69 paskaita - display cards and move data to json server

--- React/69 paskaita - 08.05 - React Router basics/sablonas/src/components/pages/Shop.jsx
+++ React/69 paskaita - 08.05 - React Router basics/sablonas/src/components/pages/Shop.jsx
@@ -1,8 +1,40 @@
+import { useContext } from "react";
+import styled from "styled-components";
+
+import ProductsContext from "../../contexts/Products";
+import ProductCard from "../UI/molecules/ProductCard";
+
+const ShopSection = styled.section`
+
+  > h1{
+    text-align: center;
+  }
+  > div{
+    display: flex;
+    justify-content: space-around;
+    gap: 10px;
+    flex-wrap: wrap;
+  }
+`;
+
 const Shop = () => {
+
+  const { products } = useContext(ProductsContext);
+
   return (
-    <section>
+    <ShopSection>
       <h1>Our Products</h1>
-    </section>
+      <div>
+        {
+          products.map(product => 
+            <ProductCard
+              key={product.id}
+              data={product}
+            />
+          )
+        }
+      </div>
+    </ShopSection>
   );
 }
  
--- React/69 paskaita - 08.05 - React Router basics/sablonas/src/index.js
+++ React/69 paskaita - 08.05 - React Router basics/sablonas/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ProductsProvider } from './contexts/Products';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>
 );
