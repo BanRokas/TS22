4cce4edfa0807c4130e326d93835457bc2514e31
BanRokas <rokas.banaitis@gmail.com>
2024-08-07 11:59:40 +0300

    71 paskaita - formik extra input fields

--- React/71 paskaita - 08.07 - React Forms using Formik/formik/src/components/pages/Register.jsx
+++ React/71 paskaita - 08.07 - React Forms using Formik/formik/src/components/pages/Register.jsx
@@ -6,7 +6,14 @@ const Register = () => {
     initialValues:{
       username: '',
       password: '',
-      passwordRepeat: ''
+      passwordRepeat: '',
+      drivingLicenseCategories: [], // checkbox
+      phone: '', // tel
+      profilePicture: '', // url
+      gender: '', // radio
+      age: '', // number
+      nationality: '', // select
+      dob: '' // date of birth
     },
     onSubmit: (values) => {
       console.log(values);
@@ -41,6 +48,113 @@ const Register = () => {
             type="password"
             name="passwordRepeat" id="passwordRepeat"
             value={formik.values.passwordRepeat}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <fieldset>
+          <legend>Driving Licenses</legend>
+          <div>
+            <label htmlFor="a">A</label>
+            <input
+              type="checkbox"
+              name="drivingLicenseCategories" id="a"
+              value='a'
+              onChange={formik.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="b">B</label>
+            <input
+              type="checkbox"
+              name="drivingLicenseCategories" id="b"
+              value='b'
+              onChange={formik.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="c">C</label>
+            <input
+              type="checkbox"
+              name="drivingLicenseCategories" id="c"
+              value='c'
+              onChange={formik.handleChange}
+            />
+          </div>
+        </fieldset>
+        <div>
+          <label htmlFor="phone">Telephone:</label>
+          <input
+            type="tel"
+            name="phone" id="phone"
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="profilePicture">Profile Picture:</label>
+          <input
+            type="url"
+            name="profilePicture" id="profilePicture"
+            value={formik.values.profilePicture}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <fieldset>
+          <legend>Gender</legend>
+          <div>
+            <label htmlFor="male">Male</label>
+            <input
+              type="radio"
+              name="gender" id="male"
+              value='male'
+              onChange={formik.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="female">Female</label>
+            <input
+              type="radio"
+              name="gender" id="female"
+              value='female'
+              onChange={formik.handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="secret">Prefer not to say</label>
+            <input
+              type="radio"
+              name="gender" id="secret"
+              value='secret'
+              onChange={formik.handleChange}
+            />
+          </div>
+        </fieldset>
+        <div>
+          <label htmlFor="age">Your age:</label>
+          <input
+            type="number"
+            name="age" id="age"
+            value={formik.values.age}
+            onChange={formik.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="nationality">Select your nationality:</label>
+          <select name="nationality" id="nationality"
+            value={formik.values.nationality} onChange={formik.handleChange}
+          >
+            <option disabled value=""></option>
+            <option value="lithuanian">Lithuanian</option>
+            <option value="latvian">Latvian</option>
+            <option value="estonian">Estonian</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="dob">Date of Birth:</label>
+          <input
+            type="datetime-local"
+            name="dob" id="dob"
+            value={formik.values.dob}
             onChange={formik.handleChange}
           />
         </div>
